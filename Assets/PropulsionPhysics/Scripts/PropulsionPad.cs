91a8daaa3be9db0f33b68531f78e161fd7b86208
pinkduh <caleb@imap.cc>
2014-12-16 12:24:11 -0800

    extracted out class for gizmos

--- Assets/PropulsionPhysics/Scripts/PropulsionPad.cs
+++ Assets/PropulsionPhysics/Scripts/PropulsionPad.cs
@@ -53,64 +53,13 @@ namespace Polycrime
         {
             if (showTrajectory && PropulsionPadActive())
             {
-                DrawTrajectory();
+                TrajectoryLine.Render(transform.position, target.position, reachTime, trajectoryColor);
             }
         }
 
         private bool PropulsionPadActive()
         {
             return (enabled && target != null && reachTime > 0);
-        }
-
-        private void DrawTrajectory()
-        {
-            Vector3 initialVelocity = TrajectoryMath.CalculateVelocity(transform.position, target.position, reachTime);
-            float deltaTime = reachTime / initialVelocity.magnitude;
-            int drawSteps = (int)(initialVelocity.magnitude - 0.5f);
-            Vector3 currentPosition = transform.position;
-            Vector3 previousPosition = currentPosition;
-            Gizmos.color = trajectoryColor;
-
-            if (IsParabolicVelocity(initialVelocity))
-            {
-                for (int i = 0; i < drawSteps; i++)
-                {
-                    currentPosition += (initialVelocity * deltaTime) + (0.5f * Physics.gravity * deltaTime * deltaTime);
-                    initialVelocity += Physics.gravity * deltaTime;
-                    Gizmos.DrawLine(previousPosition, currentPosition);
-
-                    //////////////////////////////////////////////////////////////////////////////////
-                    // If the next loop is the last iteration, then don't update the previous position
-                    // vector so it can be used to draw the gizmos arrow.
-                    if ((i + 1) < drawSteps)
-                    {
-                        previousPosition = currentPosition;
-                    }
-                }
-                DrawArrow(previousPosition, (currentPosition - previousPosition));
-            }
-            else
-            {
-                Vector3 newUpDirection = new Vector3(currentPosition.x, target.position.y, currentPosition.z);
-                Gizmos.DrawLine(currentPosition, newUpDirection);
-                DrawArrow(newUpDirection, new Vector3(0f, 0.01f, 0f));
-            }
-        }
-
-        private void DrawArrow(Vector3 position, Vector3 direction)
-        {
-            int[] arrowAngles = new int[] { 225, 135 };
-
-            foreach (int angle in arrowAngles)
-            {
-                Vector3 endPoint = Quaternion.LookRotation(direction) * Quaternion.Euler(0, angle, 0) * Vector3.forward;
-                Gizmos.DrawRay(position + direction, endPoint * 0.5f);
-            }
-        }
-
-        private bool IsParabolicVelocity(Vector3 velocity)
-        {
-            return !(velocity.x == 0 && velocity.z == 0);
         }
     }
 }

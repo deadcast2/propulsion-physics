0244b14a27f0133979a173678b95e1a6693414d1
pinkduh <caleb@imap.cc>
2014-12-15 20:41:37 -0800

    added propel back

---
+++ Assets/PropulsionPhysics/Scripts/PropelRigidbody.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Polycrime
+{
+    public class PropelRigidbody : MonoBehaviour, IPropelBehavior
+    {
+        public void React(Vector3 velocity)
+        {
+            if (rigidbody)
+            {
+                rigidbody.velocity = velocity;
+            }
+        }
+    }
+}
